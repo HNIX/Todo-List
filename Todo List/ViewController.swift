0a5e7897998a078e9ab450661fec7deceb37ef8f
Hunter Nix <hunternix@tias-macbook-pro.local>
2019-02-10 14:35:06 -0600

    Initial Commit

---
+++ Todo List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo List
+//
+//  Created by Hunter Nix on 2/10/19.
+//  Copyright © 2019 Hunter Nix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
